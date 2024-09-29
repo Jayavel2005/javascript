ce56408bed5ca5c5394e3e35577f28a2a2b98d83
Jayavel <jayavel1520@gmail.com>
2024-09-29 22:32:56 +0530

    object displaying in html web page using write() and getting user input using prompt

--- dom_properties_methods/main.js
+++ dom_properties_methods/main.js
@@ -4,12 +4,18 @@ const person = {
   age: 30,
   city: "New York",
   greet() {
-    return `Hello my name is ${this.name}.I am ${this.age} year's old.I am living in ${this.city}`;
+    return `Hello my name is ${this.name}.I am ${this.age} years old.I am living in ${this.city}`;
   },
 };
 
-person.name = "Jayavel";
-person.city = "Vadambacheri";
-person.age = 19;
+person.name = window.prompt("Enter you name: ","Jayavel");  // prompt() -> has two parameters message string and default value
+person.age = window.prompt("Enter your age: ");
+
 const personOne = person.greet();
 console.log(personOne);
+
+// write method
+
+document.body.innerHTML = `<h1>${personOne}</h1>`;
+
+document.write(personOne);
