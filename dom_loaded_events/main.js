c86815a87f25d957cc1337a0543302777c5bb4d8
Jayavel <jayavel1520@gmail.com>
2024-09-29 23:00:00 +0530

    dom content loaded event

---
+++ dom_loaded_events/main.js
@@ -0,0 +1,8 @@
+// DOM loaded events
+document.addEventListener("DOMContentLoaded", function () {
+  alert("DOM content loaded");
+});
+
+const header = document.getElementById("headerid");
+header.innerHTML = "Jayavel";
+console.log(header);
