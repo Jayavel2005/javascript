889346b7c3ec876ad47fef8a3ec963afb74014a6
Jayavel <jayavel1520@gmail.com>
2024-09-29 23:17:05 +0530

    getting image height and width

--- dom_loaded_events/main.js
+++ dom_loaded_events/main.js
@@ -1,8 +1,15 @@
 // DOM loaded events
-document.addEventListener("DOMContentLoaded", function () {
-  alert("DOM content loaded");
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   alert("DOM content loaded");
+// });
 
-const header = document.getElementById("headerid");
-header.innerHTML = "Jayavel";
-console.log(header);
+// const header = document.getElementById("headerid");
+// header.innerHTML = "Jayavel";
+// console.log(header);
+
+// we can get image width and height using offetheight and offsetWidth
+
+const natureImage = document.getElementById("image");
+
+console.log(natureImage.offsetWidth);
+console.log(natureImage.offsetHeight);
