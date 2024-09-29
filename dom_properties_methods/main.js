3a546d40d484cd2a07bb2c0237fa42f412cbcef1
Jayavel <jayavel1520@gmail.com>
2024-09-29 22:18:32 +0530

    updated

--- dom_properties_methods/main.js
+++ dom_properties_methods/main.js
@@ -1,9 +1,15 @@
-// object example 
+// object example
 const person = {
   name: "John Doe",
   age: 30,
   city: "New York",
-  greet: function () {
-    return `Hello my name is ${this.name}.I am ${this.age} year's old.I am living in ${this.city}`
+  greet() {
+    return `Hello my name is ${this.name}.I am ${this.age} year's old.I am living in ${this.city}`;
   },
 };
+
+person.name = "Jayavel";
+person.city = "Vadambacheri";
+person.age = 19;
+const personOne = person.greet();
+console.log(personOne);
