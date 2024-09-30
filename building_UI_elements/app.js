3dab8068c9c02122d1b8f663d8212074ed90206c
Jayavel <jayavel1520@gmail.com>
2024-09-30 20:31:56 +0530

    building ui elemetns

---
+++ building_UI_elements/app.js
@@ -0,0 +1,7 @@
+// DOM selector
+// 1.Get element by Id 
+// get element by class name
+// get element by tag name
+// get element by query selector
+// finding elements using css selector
+// dom query selector
