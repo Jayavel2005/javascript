da26bf6e21ee1d0d6025fef734b300b17628e9a8
Jayavel <jayavel1520@gmail.com>
2024-10-02 12:46:41 +0530

    varibles finished

---
+++ fundementals/varibales.js
@@ -0,0 +1,21 @@
+// // variables are used to store values it is also like a container
+
+// // var x = 30;
+// // var x = "hello world";
+// // console.log(x);
+
+// // let is a block scoped variable declration
+
+// // let x = 10;
+// // if(x > 5){
+// //     let y = 20;
+// //     // console.log(y)
+// // }
+// // console.log(y);
+
+
+// // const variable declaration is used to create constant elements
+
+// const a = 10;
+// a = 5;
+// console.log(a)
