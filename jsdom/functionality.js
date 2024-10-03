0a877dd7dac52b5719609147bdd90b3521e7c0c7
Jayavel <jayavel1520@gmail.com>
2024-10-03 22:32:57 +0530

    functionalities learned of inserting elements

--- jsdom/functionality.js
+++ jsdom/functionality.js
@@ -11,3 +11,13 @@
 // // document fragment for performance
 
 // when appending a new element the page will reflows to prevent this fragment is used 
+
+// insertAdjacentElement
+// insertAdjacentHtml
+const foodItems = document.getElementById("food-items");
+
+const newfood = document.createElement("li");
+const inputFood = document.createTextNode(prompt("Enter the food item Name: "))
+newfood.append(inputFood);
+newfood.className = "food-item";
+foodItems.insertAdjacentHTML("beforeend",`<li class="food-item">${newfood.textContent}</li>`)
